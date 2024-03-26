0a25a3f019cacd21cc6d9e60082f0df934c4fb2a
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-26 23:59:03 +1300

    q2 not going so well

--- secondQ.js
+++ secondQ.js
@@ -1,4 +1,27 @@
-// there is a list of numbers starting at 'L' and ending at 'R'. 
+// there is an list of numbers starting at 'L' and ending at 'R'.
 // these are consective numbers in a list, but you dont know the exact number
 // some of these numbers will be odd
 // the task is to take all these numbers and publish them in an array
+
+// L = start of list
+// R = end of list
+// n = number of numbers in the list
+// arr = array
+originalList = '6,7,8,9,10'
+
+// TODO
+// put the L to R list in to an array
+// create a filter function that takes in the array and returns the odd numbers
+
+function stringToIntArray(originalList) {
+  const strArray = str.split(',')
+  const intArray = strArray.map((num) => parseInt(num))
+  return intArray
+}
+
+function isOdd(num) {
+  return num % 2 !== 0
+}
+
+const oddsOnly = intArray.filter(isOdd)
+console.log(oddsOnly)
