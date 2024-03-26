b113c5d2873420eb627f8cf5e60322eae745439b
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-26 22:34:56 +1300

    question 2 question added

---
+++ secondQ.js
@@ -0,0 +1,4 @@
+// there is a list of numbers starting at 'L' and ending at 'R'. 
+// these are consective numbers in a list, but you dont know the exact number
+// some of these numbers will be odd
+// the task is to take all these numbers and publish them in an array
